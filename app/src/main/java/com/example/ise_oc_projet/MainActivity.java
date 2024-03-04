91937725244d26817f9a7ea980d165c48e1cc385
MrRenkyu <levillain.enzo@outlook.fr>
2024-03-04 12:27:40 +0100

    autorisation et activation BT

--- app/src/main/java/com/example/ise_oc_projet/MainActivity.java
+++ app/src/main/java/com/example/ise_oc_projet/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.ise_oc_projet;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    BluetoothConnector BT_conn;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BT_conn = new BluetoothConnector(this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        BT_conn.activityResultReceived(resultCode);
     }
 }

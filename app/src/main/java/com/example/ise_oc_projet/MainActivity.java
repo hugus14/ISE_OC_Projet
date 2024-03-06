304bd419838c72e6feaf1882bde42a234433cee5
Vaas <hugo.lm2956@gmail.com>
2024-03-06 10:08:23 +0100

    ajout connectivity manager

--- app/src/main/java/com/example/ise_oc_projet/MainActivity.java
+++ app/src/main/java/com/example/ise_oc_projet/MainActivity.java
@@ -2,15 +2,44 @@ package com.example.ise_oc_projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //*** VARIABLES ***//
+    Button btnConnection;
+    ConnectivityManager connectivityManager;
+    NetworkInfo networkInfo;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //4
+        btnConnection = findViewById(R.id.button);
+        tv = findViewById(R.id.textView);
+
+        btnConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+                networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if(networkInfo != null) tv.setText(networkInfo.getTypeName());
+                else tv.setText("Pas de réseau");
+
+            }
+        });
+
+
+
+
     }
 }
